a07df60289c2b58a1f18f98d96a69c36325eee95
CorteZzz137 <CorteZzz137@users.noreply.github.com>
2022-10-04 18:58:15 +0000

    deploy: c00200665a026e51a7fc920e8cad3fe332f17f52

--- _app/immutable/chunks/1-bc85fcdb.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-37fe58d9.js";export{t as component};
---
+++ _app/immutable/chunks/2-2bc5f5a0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-df20a6a8.js";export{t as component};
