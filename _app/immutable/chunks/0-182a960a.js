4e3c27cb39a958d97c8f492605d229cd327b44ed
CorteZzz137 <CorteZzz137@users.noreply.github.com>
2022-11-20 17:50:28 +0000

    deploy: ffb642617a0857415573992792e42dd07be6bbeb

---
+++ _app/immutable/chunks/0-182a960a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-01c23f51.js";const e=!0;export{t as component,e as server};
---
+++ _app/immutable/chunks/2-48e41437.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-2906c3fa.js";export{t as component};
