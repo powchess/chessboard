86a0d3a3e57ec304192b934681cc133ce49ce458
Alexei Torgashov <alexei.torgashov@yahoo.de>
2023-07-03 20:33:09 +0200

    update app.d.ts for correct CustomEvent types

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,24 +6,23 @@ declare namespace App {
 	// interface PublicEnv {}
 }
 
-declare namespace svelte.JSX {
-	// eslint-disable-next-line
-	interface HTMLProps<T> {
-		onstartMoving?: (e: CustomEvent) => void;
-		onsquareover?: (e: CustomEvent) => void;
+declare namespace svelteHTML {
+	interface HTMLAttributes {
+		'on:startMoving'?: (e: CustomEvent) => void;
+		'on:squareover'?: (e: CustomEvent) => void;
 		onanimationEnded?: (e: CustomEvent) => void;
 		oncaptured?: (e: CustomEvent) => void;
-		ondropped?: (e: CustomEvent) => void;
-		onclicked?: (e: CustomEvent) => void;
+		'on:dropped'?: (e: CustomEvent) => void;
+		'on:clicked'?: (e: CustomEvent) => void;
 		oncanceled?: (e: CustomEvent) => void;
 		onmovemadeUndo?: (e: CustomEvent) => void;
 		onpieceCaptured?: (e: CustomEvent) => void;
 		onmovemadeSilent?: (e: CustomEvent) => void;
 		onnewPreMove?: (e: CustomEvent) => void;
 		onerror?: (e: ErrorEvent) => void;
-		onresizing?: (e: CustomEvent) => void;
-		onendResizing?: (e: CustomEvent) => void;
-		ondrawCircle?: (e: CustomEvent) => void;
-		ondrawArrow?: (e: CustomEvent) => void;
+		'on:resizing'?: (e: CustomEvent) => void;
+		'on:endResizing'?: (e: CustomEvent) => void;
+		'on:drawCircle'?: (e: CustomEvent) => void;
+		'on:drawArrow'?: (e: CustomEvent) => void;
 	}
 }
