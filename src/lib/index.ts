823ffd04d7f7b765096671b1561d88810644d152
Alexei Torgashov <alexei.torgashov@yahoo.de>
2023-11-05 14:47:44 +0100

    export showcasepiece

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 import Chessboard from './Chessboard.svelte';
 import type { ChessboardConfig } from './boardConfig.js';
+import ShowcasePiece from './ShowcasePiece.svelte';
 
-export { type ChessboardConfig };
+export { type ChessboardConfig, ShowcasePiece };
 export default Chessboard;
