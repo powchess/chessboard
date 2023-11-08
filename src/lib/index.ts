a4943fa59142dc8c53ba29d09f984c8452af7dc3
CorteZzz137 <cortezzz137@gmail.com>
2023-11-08 17:57:21 +0500

    Add Pieces to exported module.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 import Chessboard from './Chessboard.svelte';
 import type { ChessboardConfig } from './boardConfig.js';
 import ShowcasePiece from './ShowcasePiece.svelte';
+import { Pieces } from './state/piece.js';
 
-export { type ChessboardConfig, ShowcasePiece };
+export { type ChessboardConfig, ShowcasePiece, Pieces };
 export default Chessboard;
