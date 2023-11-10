79d39d45425992ba42cf8062d8a423f30f4763cd
Alexei Torgashov <alexei.torgashov@yahoo.de>
2023-11-10 16:48:41 +0100

    add seperate export path for pieces

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,6 @@
 import Chessboard from './Chessboard.svelte';
 import type { ChessboardConfig } from './boardConfig.js';
 import ShowcasePiece from './ShowcasePiece.svelte';
-import { Pieces } from './state/piece.js';
 
-export { type ChessboardConfig, ShowcasePiece, Pieces };
+export { type ChessboardConfig, ShowcasePiece };
 export default Chessboard;
